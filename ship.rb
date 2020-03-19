b39613a0e8e35a1cd5b3f7d1d1d166a4ffc297ea
Aaron Campbell <sharkbeardio@gmail.com>
2020-03-18 23:08:32 -0400

    add size to ship

--- ship.rb
+++ ship.rb
@@ -10,4 +10,14 @@ class Ship
   def initialize(x: 0, y: 0, type: 0)
     @type = type
   end
+
+  def size
+    case @type
+    when CARRIER then 5
+    when BATTLESHIP then 4
+    when SUBMARINE then 3
+    when DESTROYER then 3
+    when PATROL then 2
+    end
+  end
 end
--- ship_spec.rb
+++ ship_spec.rb
@@ -1,7 +1,23 @@
 require './ship'
 
 describe Ship, 'new' do
-  it 'creates a ship of specified type' do
-    expect(Ship.new(type: Ship::BATTLESHIP).type).to eq(Ship::BATTLESHIP)
+  context 'when creating a battleship' do
+    it 'creates a ship of specified type' do
+      expect(Ship.new(type: Ship::BATTLESHIP).type).to eq(Ship::BATTLESHIP)
+    end
+
+    it 'has a size of 4' do
+      expect(Ship.new(type: Ship::BATTLESHIP).size).to eq(4)
+    end
+  end
+
+  context 'when creating a carrier' do
+    it 'is of the carrier type' do
+      expect(Ship.new(type: Ship::CARRIER).type).to eq(Ship::CARRIER)
+    end
+
+    it 'has a size of 5' do
+      expect(Ship.new(type: Ship::CARRIER).size).to eq(5)
+    end
   end
 end
