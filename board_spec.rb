8a3bcede7f2fc3ecc7a75d2e5e1a2e9169b832e1
Aaron Campbell <sharkbeardio@gmail.com>
2020-03-19 04:34:20 -0400

    refactor ship spec

--- board_spec.rb
+++ board_spec.rb
@@ -7,10 +7,12 @@ RSpec.describe Board, '#new' do
     it 'initializes a board with specified size' do
       expect(board_size_12.size).to eq(12)
     end
+
     it 'does not create a default size board' do
       expect(board_size_12.size).to_not eq(10)
     end
   end
+
   context 'when given no params' do
     it 'creates a size 10 board' do
       expect(Board.new.size).to eq(10)
--- ship_spec.rb
+++ ship_spec.rb
@@ -1,23 +1,29 @@
 require './ship'
 
 describe Ship, 'new' do
+  let(:patrol) { Ship.new(type: Ship::PATROL) }
+  let(:destroyer) { Ship.new(type: Ship::DESTROYER) }
+  let(:submarine) { Ship.new(type: Ship::SUBMARINE) }
+  let(:battleship) { Ship.new(type: Ship::BATTLESHIP) }
+  let(:carrier) { Ship.new(type: Ship::CARRIER) }
+
   context 'when creating a battleship' do
     it 'creates a ship of specified type' do
-      expect(Ship.new(type: Ship::BATTLESHIP).type).to eq(Ship::BATTLESHIP)
+      expect(battleship.type).to eq(Ship::BATTLESHIP)
     end
 
     it 'has a size of 4' do
-      expect(Ship.new(type: Ship::BATTLESHIP).size).to eq(4)
+      expect(battleship.size).to eq(4)
     end
   end
 
   context 'when creating a carrier' do
     it 'is of the carrier type' do
-      expect(Ship.new(type: Ship::CARRIER).type).to eq(Ship::CARRIER)
+      expect(carrier.type).to eq(Ship::CARRIER)
     end
 
     it 'has a size of 5' do
-      expect(Ship.new(type: Ship::CARRIER).size).to eq(5)
+      expect(carrier.size).to eq(5)
     end
   end
 end
