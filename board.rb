182ecc021d750daadb58547e5d7206a4c22620c3
Aaron Campbell <sharkbeardio@gmail.com>
2020-03-19 17:41:38 -0400

    check for shots outside of board, or same space

--- board.rb
+++ board.rb
@@ -4,9 +4,15 @@ class Board
   def initialize(size: 10)
     @size = size
     @shots = Array.new
+    @safe_range = 0...size
   end
   
   def fire(x, y)
+    [x, y].each do |var|
+      raise ArgumentError.new('outside of board') if @safe_range.none?(var)
+    end
+    raise ArgumentError.new('already shot at the space') if @shots.include?([x, y])
+      
     @shots << [x, y]
   end
 end
--- board_spec.rb
+++ board_spec.rb
@@ -23,12 +23,33 @@ RSpec.describe Board, '#new' do
 end
 
 RSpec.describe Board, '#fire' do
-  context 'when taking a shot' do
-    let(:board) { Board.new }
+  let(:board) { Board.new }
 
+  context 'when taking a shot' do
     it 'stores shots' do
       board.fire(3, 2)
       expect(board.shots).to include([3, 2])
+    end
+  end
+
+  context 'when shooting negative coords' do
+    it 'throws an exception' do
+      expect { board.fire(-1, 0) }.to raise_exception('outside of board')
+    end
+  end
+
+  context 'when shooting out of range' do
+    it 'throws an exception' do
+      expect { board.fire(0, 55) }.to raise_exception('outside of board')
+    end
+
+    it 'throws an exception when equal to size' do
+      expect { board.fire(10, 3) }.to raise_exception('outside of board')
+    end
+
+    it 'throws an exception if shooting the same place twice' do
+      board.fire(1, 1)
+      expect { board.fire(1, 1) }.to raise_exception('already shot at the space')
     end
   end
 end
