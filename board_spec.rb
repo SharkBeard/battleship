cdc6526a1ed183f145436d61928c531073507e36
Aaron Campbell <sharkbeardio@gmail.com>
2020-03-19 05:02:48 -0400

    clean up board spec

--- board_spec.rb
+++ board_spec.rb
@@ -1,21 +1,23 @@
 require './board'
 
 RSpec.describe Board, '#new' do
-  let(:board_size_12) { Board.new(size: 12) }
-
   context 'when given a size' do
+    let(:board) { Board.new(size: 12) }
+
     it 'initializes a board with specified size' do
-      expect(board_size_12.size).to eq(12)
+      expect(board.size).to eq(12)
     end
 
     it 'does not create a default size board' do
-      expect(board_size_12.size).to_not eq(10)
+      expect(board.size).to_not eq(10)
     end
   end
 
   context 'when given no params' do
+    let(:board) { Board.new }
+
     it 'creates a size 10 board' do
-      expect(Board.new.size).to eq(10)
+      expect(board.size).to eq(10)
     end
   end
 
