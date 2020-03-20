782019ca604b84be7bb22fa7511077edd01bd179
Aaron Campbell <sharkbeardio@gmail.com>
2020-03-20 04:16:28 -0400

    add cells, hit, and status to Ship

--- ship.rb
+++ ship.rb
@@ -7,16 +7,35 @@ class Ship
 
   attr_accessor :type
 
-  def initialize(x: 0, y: 0, type: 0)
+  def initialize(x: 0, y: 0, type: 0, direction: :horizontal)
     @type = type
+    @health = size
+    assign_position(x: x, y: y, direction: direction, length: size)
+  end
+
+  def hit
+    @health -= 1
+  end
+
+  def status
+    if @health <= 0
+      :destroyed
+    elsif @health >= size
+      :pristine
+    else
+      :damaged
+    end
   end
 
   def size
     @size ||= calculate_size
   end
 
-  private
+  def cells
+    @cells ||= generate_cell_list
+  end
 
+  private
   def calculate_size
     case @type
     when CARRIER then 5
@@ -24,6 +43,33 @@ class Ship
     when SUBMARINE then 3
     when DESTROYER then 3
     when PATROL then 2
+    end
+  end
+
+  def generate_cell_list
+    cells = []
+    if @x.is_a? Enumerable
+      @x.each do |x|
+        cells << [x, @y]
+      end
+    elsif @y.is_a? Enumerable
+      @y.each do |y|
+        cells << [@x, y]
+      end
+    else
+      cells << [@x, @y]
+    end
+    cells
+  end
+
+  def assign_position(x:, y:, direction:, length:)
+    case direction
+    when :horizontal
+      @x = x...(x+length)
+      @y = y
+    when :vertical
+      @x = x
+      @y = y...(y+length)
     end
   end
 end
--- ship_spec.rb
+++ ship_spec.rb
@@ -1,11 +1,11 @@
 require './ship'
 
 describe Ship, 'new' do
-  let(:patrol) { Ship.new(type: Ship::PATROL) }
-  let(:destroyer) { Ship.new(type: Ship::DESTROYER) }
-  let(:submarine) { Ship.new(type: Ship::SUBMARINE) }
-  let(:battleship) { Ship.new(type: Ship::BATTLESHIP) }
-  let(:carrier) { Ship.new(type: Ship::CARRIER) }
+  let(:patrol) { Ship.new(type: Ship::PATROL, x: 1, y: 2, direction: :horizontal) }
+  let(:destroyer) { Ship.new(type: Ship::DESTROYER, x: 1, y: 2, direction: :horizontal) }
+  let(:submarine) { Ship.new(type: Ship::SUBMARINE, x: 1, y: 2, direction: :horizontal) }
+  let(:battleship) { Ship.new(type: Ship::BATTLESHIP, x: 1, y: 2, direction: :vertical) }
+  let(:carrier) { Ship.new(type: Ship::CARRIER, x: 1, y: 2, direction: :horizontal) }
 
   context 'when creating a battleship' do
     it 'creates a ship of specified type' do
@@ -14,6 +14,14 @@ describe Ship, 'new' do
 
     it 'has a size of 4' do
       expect(battleship.size).to eq(4)
+    end
+
+    it 'takes up 4 cells' do
+      expect(battleship.cells.count).to eq(4)
+    end
+
+    it 'is going the right direction' do
+      expect(battleship.cells).not_to include([2, 2])
     end
   end
 
@@ -24,6 +32,23 @@ describe Ship, 'new' do
 
     it 'has a size of 5' do
       expect(carrier.size).to eq(5)
+    end
+
+    it 'has the middle cell' do
+      expect(carrier.cells).to include([3, 2])
+    end
+  end
+end
+
+RSpec.describe Ship, '#status' do
+  let(:battleship) { Ship.new(type: Ship::BATTLESHIP, x: 1, y: 2, direction: :vertical) }
+
+  context 'when a battleship is hit 4 times' do
+    it 'has a status of destroyed' do
+      4.times do
+        battleship.hit
+      end
+      expect(battleship.status).to eq(:destroyed)
     end
   end
 end
